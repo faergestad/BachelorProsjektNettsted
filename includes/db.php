510ec138725343fdca7d6567f5a1aff475ce8c8a
faergestad <georg.f.persen@gmail.com>
2018-02-13 00:53:31 +0100

    Delete db.php

--- includes/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-   define("SERVER", "localhost");
-   define("USERNAME", "root");
-   define("PASSWORD", "");
-   define("DATABASE", "mydb");
-   $db = mysqli_connect(SERVER,USERNAME,PASSWORD,DATABASE) or die("Kunne ikke koble til db-server");
-   $db->set_charset("utf8");
-
-?>
