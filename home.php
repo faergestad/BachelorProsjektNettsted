14a8c7f7e72f1418d3e2acc9c1925df0612d7cf4
leite95 <leite.andreas@gmail.com>
2018-03-07 12:05:53 +0100

    Kjetil's fuck opp

--- home.php
+++ home.php
@@ -39,19 +39,10 @@
 <div class="container">
   <div class="row">
     <div class="col-sm-4">
-      <h3>Column 1</h3>
-      <p>Lorem ipsum dolor..</p>
-      <p>Ut enim ad..</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor..</p>
-      <p>Ut enim ad..</p>
-    </div>
-    <div class="col-sm-4">
-      <h3>Column 3</h3> 
-      <p>Lorem ipsum dolor..</p>
-      <p>Ut enim ad..</p>
+      <h2>Hjem</h3>
+      <p>asd asd asd asda da asd asd</p>
+      <p>asdasd sada d..</p>
+
     </div>
   </div>
 </div>
